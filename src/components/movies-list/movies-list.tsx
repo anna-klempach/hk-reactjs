438ff6ea0fa946d5213fa635ab11e72cd3651c65
Hanna Klempach <hanna_klempach@epam.com>
2021-03-07 03:12:00 +0300

    task-4 / Implement modal window
    task-4 / Add filters
    
    task-4 / Implement add movie functionality
    
    task-4 / Implement delete movie
    
    task-4 / Add lifecycle hooks
    
    task-4 / Remove unnecessary lifecycle method
    
    task-4 / Fix minor issues
    
    task-5 / Change class components into functional components
    
    task-5: Add custom effects
    
    task-5 / Add movie details page
    
    task-5 / Fix search

--- src/components/movies-list/movies-list.tsx
+++ src/components/movies-list/movies-list.tsx
@@ -1,39 +1,55 @@
 import * as React from 'react';
 import './movies-list.scss';
+import { useCallback } from 'react';
 import MovieCard from '../movie-card/movie-card';
-import { Movie } from '../../models/movie';
+import { Movie, MovieQueryParams } from '../../models/movie';
+import useFetch from '../../hooks/use-fetch';
+import { SortingDirectionEnum, SortingFieldsEnum } from '../../models/enums/movies-list';
 
 export interface MoviesListProps {
-  moviesList: Movie[],
-  onMovieCardClick: (id: number) => void
+  onMovieCardClick: (id: number) => void,
+  movieQueryParams: MovieQueryParams
 }
 
-function MoviesList(props: MoviesListProps): React.ReactElement {
-  const handleMovieCardClick = (id: number) => {
-    props.onMovieCardClick(id);
-  }
+function MoviesList({ onMovieCardClick, movieQueryParams }: MoviesListProps): React.ReactElement {
 
-  if (props.moviesList.length > 0) {
-    return (
-      <>
+  // TODO: UseFetch seems promising, especially for getting separate movies
+  // Need to refactor it as soon as redux is implemented
+  const [url, setUrl] = React.useState('');
+  const [init, setInit] = React.useState(null);
+  const [status, moviesList] = useFetch<Array<Movie>>({ url, init });
+
+  React.useEffect(() => {
+    setUrl(`http://localhost:4000/movies?${new URLSearchParams(movieQueryParams)}`);
+    setInit({
+      method: 'GET'
+    });
+  }, [movieQueryParams]);
+
+  const handleMovieCardClick = useCallback(
+    (id: number) => {
+      onMovieCardClick(id)
+    }, [moviesList]
+  );
+
+  return (
+    Array.isArray(moviesList) && moviesList.length !== 0
+      ? <>
         <p className="movies-total">
-          <span className="movies-total__count">{props.moviesList.length}</span> movies found.
+          <span className="movies-total__count">{moviesList.length}</span> movies found.
             </p>
         <div className="movies-list">
           {
-            props.moviesList.map(movie => <MovieCard movie={movie} key={movie.id}
+            moviesList.map(movie => <MovieCard movie={movie} key={movie.id}
               onMovieCardClick={e => handleMovieCardClick(e)}
               />)
           }
         </div>
       </>
+      : <div className="movies-not-found-message">
+        <p>No Movies Found</p>
+      </div>
     );
-  }
-  return (
-    <div className="movies-not-found-message">
-      <p>No Movies Found</p>
-    </div>
-  )
 }
 
 export default MoviesList;
